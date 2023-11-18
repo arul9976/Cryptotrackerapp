22c7afc43bf1566c6edbd95dea77515fa21bdb6b
Arulkumar M <79500043+arul9976@users.noreply.github.com>
2023-11-18 18:38:58 +0530

    Update01 script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const CryptoJson = (url) => {
     })
         .then(response => response.json())
         .then(data => {
-            console.log('data', data)
             containBox(data)
         })
         .catch(err => {
@@ -51,7 +50,6 @@ const CryptoAllJson = (url) => {
         .then(response => response.json())
         .then(data => {
             // LoadData(data, 0)
-            console.log(data,'gtgt')
             data.forEach(DataOn => {
                 LoadData(DataOn, data.indexOf(DataOn))
             })
@@ -116,7 +114,7 @@ const LoadData = (data, indexNum) => {
             })
 
             localStorage.setItem('ids', JSON.stringify(ids))
-            console.log('hihiih')
+            console.log('done')
         }
         else {
 
